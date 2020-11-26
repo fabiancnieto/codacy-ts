ed8a9b838a8cf249b70acc49fda0e80d62995d56
fabian nieto <fabian.nieto@deliveryhero.com>
2020-11-26 17:41:37 +0100

    add tsconfig

--- index.js
+++ index.js
@@ -1,12 +1,1 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const express = require("express");
-const app = express();
-app.post("/v1/menu-import-log", (req, res, next) => {
-    console.log(req.headers, req.body);
-    res.status(200).send({ header: req.headers, body: req.body });
-});
-app.listen(8080, (error) => {
-    console.log(error);
-});
 //# sourceMappingURL=index.js.map
